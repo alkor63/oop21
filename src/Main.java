081205b205eb9bd3ce0e16f2753532ca7d2e90fc
alkor63 <alkor@medi-spa.ru>
2023-01-24 23:18:16 +0300

    HomeWork 10

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,47 @@
+import java.util.Random;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bye world!");
 
-        Predicate<Double> x = new Predicate<Double>() {
+        System.out.println("*******  ДЗ 10 задача 1  *******");
+        java.util.Random random = new java.util.Random();
+        int iNum = random.nextInt(100) - 50; // генерируем целое число от -50 до 50
+        Predicate<Integer> p = new Predicate<Integer>() {
             @Override
-            public boolean test(Double v) {
-                return v >= 0.0;
+            public boolean test(Integer v) {
+                return v >= 0;
             }
         };
+        String s;
+        if (p.test(iNum)) s = " положительное";
+        else s = " отрицательное";
+        System.out.println("Число " + iNum + s);
+
+        Predicate<Integer> pp = v -> v >= 0;
+        System.out.println("--- реализация через лямбду: ---");
+        iNum = random.nextInt(100) - 50; // генерируем целое число от -50 до 50
+        if (pp.test(iNum)) s = " положительное";
+        else s = " отрицательное";
+        System.out.println("Число " + iNum + s);
+
+        System.out.println("\n*******  ДЗ 10 задача 2  *******");
+        String name = "Nick";
+        Consumer<String> consumer = new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("My name is " + s + ", i'm programmer");
+            }
+        };
+        consumer.accept(name);
+        System.out.println("--- реализация через лямбду: ---");
+        name = "Alex";
+        Consumer<String> consumer1 = s1 -> System.out.println("My name is " + s1 + ", i'm driver");
+        consumer1.accept(name);
+
+        System.out.println("\n*******  ДЗ 10 задача 3  *******");
+
 
     }
 
